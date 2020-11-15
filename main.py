93fddbdedd1e49ebbb6b2c5aeb1c29da9b090846
Ivan <Salik52@yandex.ru>
2020-11-15 18:55:29 +0500

    Сделано дз
    Сделал на примере с 4 акциями чтобы не тратить попусту запросы. При изменении числа в переменной count на нужное будет столько акций сколько нужно. Суммировал стоймость при закрытии в данный день. Дата берется с компа локальная.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 from constants import KEY
+import time
+from tqdm import tqdm
+
+def get_time():
+    data = time.localtime()  # получить struct_time
+    stroka = str(data.tm_year) + '-' + str(data.tm_mon) + '-' + str(data.tm_mday)
+    stroka.split(' ')
+    print(stroka + 'T00:00:00+0000')
 
 
 def main():
@@ -14,12 +22,31 @@ def price_close():
     print(r.json()['data'][0]['close'])
 
 
-def get_ticker():
+def get_top():
     payload = {'access_key': f'{KEY}'}
     r = requests.get('http://api.marketstack.com/v1/tickers', payload)
     for i in r.json()['data']:
-        print(i['name'], ' - ', i['symbol'])
+        print(i['symbol'])
+
+
+def get_top(date):
+    payload = {'access_key': f'{KEY}'}
+    r = requests.get('http://api.marketstack.com/v1/tickers', payload)
+    fullpricetop = 0
+    count = 4
+    g = 0
+    with tqdm(total=count) as pbar:
+        for i in r.json()['data']:
+            if g < count:
+                payload = {'symbols': f'{i["symbol"]}', 'access_key': f'{KEY}', 'limit': 1, 'data': date}
+                r = requests.get('http://api.marketstack.com/v1/eod', payload)
+                fullpricetop += r.json()['data'][0]['close']
+                pbar.update(1)
+                g += 1
+            else:
+                print(fullpricetop)
+                break
 
 
 if __name__ == '__main__':
-    get_ticker()
+    get_top(get_time())
