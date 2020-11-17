41ff9bebad8f92af928b7667b36bfee87902fb8a
Ivan <Salik52@yandex.ru>
2020-11-17 14:23:41 +0000

    fix for in code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from constants import KEY
 import time
+import math
 from tqdm import tqdm
 
 def get_time():
@@ -22,30 +23,54 @@ def price_close():
     print(r.json()['data'][0]['close'])
 
 
-def get_top():
+def get_top_w():
     payload = {'access_key': f'{KEY}'}
     r = requests.get('http://api.marketstack.com/v1/tickers', payload)
     for i in r.json()['data']:
         print(i['symbol'])
 
+"""
+limit = a 
+offset = b 
+b, b+1, b+2, ... b+a-1 
+limit = 3
+надо набрать 10 акций
+offset = 0, 3, 6, 9
+акции = 0 1 2, 3 4 5, 6 7 8, 9 10 [11]
+100
+count_of_req = 34
+34
+i+3
+"""
+def get_symbols(count): 
+    """
+        count - кол-во акций которые надо получить 
 
-def get_top(count, date):
-    payload = {'access_key': f'{KEY}'}
-    r = requests.get('http://api.marketstack.com/v1/tickers', payload)
+        return - массив из symbols например ['APPL', 'MSFT', ....]
+    """
+    limit = 3
+    offset = 0
+    list_of_tickers = []
+    count_of_requests = math.ceil(count/limit)
+    for i in range(count_of_requests):
+        payload = {'access_key': f'{KEY}', 'limit':limit, 'offset': offset}
+        r = requests.get('http://api.marketstack.com/v1/tickers', payload)
+        for j in r.json()['data']:
+            list_of_tickers.append(j['symbol'])
+        offset += limit
+    list_of_tickers = list_of_tickers[:count]
+    return list_of_tickers
+
+
+def get_fullprice_of_top(count, date):
     fullpricetop = 0
-    with tqdm(total=count) as pbar:
-        tickers = r.json()['data']
-        for i, ticker in enumerate(tickers):
-            if i < count:
-                payload = {'symbols': f'{ticker["symbol"]}', 'access_key': f'{KEY}', 'limit': 1, 'data': date}
-                r = requests.get('http://api.marketstack.com/v1/eod', payload)
-                fullpricetop += r.json()['data'][0]['close']
-                pbar.update(1)
-            else:
-                break
-    print(fullpricetop)
+    for ticker in tqdm(get_symbols(count)):
+        payload = {'symbols': ticker, 'access_key': f'{KEY}', 'limit': 1, 'data': date}
+        r = requests.get('http://api.marketstack.com/v1/eod', payload)
+        fullpricetop += r.json()['data'][0]['close']           
+    return fullpricetop
 
 
 if __name__ == '__main__':
     count_of_tickers = int(input('Введите желаемое кол-во акций: '))
-    get_top(count_of_tickers, get_time())
+    print(get_fullprice_of_top(count_of_tickers, get_time()))
